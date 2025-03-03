65749b94113b00febb9905ac1e14e5981832906f
Johnnae Curry <curryjo1@msu.edu>
2025-03-03 05:40:53 +0000

    commit please

---
+++ scroll-reveal.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    ScrollReveal().reveal('.reveal', {
+        distance: '50px',
+        duration: 1000,
+        easing: 'ease-in-out',
+        origin: 'bottom',
+        opacity: 0,
+        delay: 300
+      })})
